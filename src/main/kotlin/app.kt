0b39ed2ab3dfbb0a92586858715fc25038daeb07
Alex Selyutin <selutin.alexandr90@gmail.com>
2025-05-25 15:01:06 +0300

    while

--- src/main/kotlin/app.kt
+++ src/main/kotlin/app.kt
@@ -1,2 +1,14 @@
 package org.example
 
+fun main() {
+
+    var x = 1
+    println("Before the loop X = $x")
+    while (x < 4) {
+        println("In the loop X = $x")
+        x = x + 1
+    }
+
+    println("After the loop X = $x")
+
+}
