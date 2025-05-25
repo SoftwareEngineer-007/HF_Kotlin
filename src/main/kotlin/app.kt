eb33277d43d8526fc009836dd019edd4480de7a6
Alex Selyutin <selutin.alexandr90@gmail.com>
2025-05-25 15:59:57 +0300

    YabbaDabbaDo

--- src/main/kotlin/app.kt
+++ src/main/kotlin/app.kt
@@ -1,10 +1,18 @@
 package org.example
 
 fun main() {
-
+/*
     val x = 4
     val y = 1
     println (if (x > y) "x is greater than y" else "x is not greater than y")
     println("this line runs no matter what")
+*/
 
+    var a = 1
+    while (a < 3) {
+        print(if (a == 1) "Yab" else "Dab")
+        print("ba")
+        a = a + 1
+    }
+    if (a == 3) println("Do")
 }
