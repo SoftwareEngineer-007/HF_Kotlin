3c3ad820e565124adf12f16aeacd66633d5ab6b9
Alex Selyutin <selutin.alexandr90@gmail.com>
2025-05-25 15:52:37 +0300

    new_if_else

--- src/main/kotlin/app.kt
+++ src/main/kotlin/app.kt
@@ -4,11 +4,7 @@ fun main() {
 
     val x = 4
     val y = 1
-    if (x > y) {
-        println("x is greater than y")
-    } else {
-        println("x is not greater than y")
-    }
+    println (if (x > y) "x is greater than y" else "x is not greater than y")
     println("this line runs no matter what")
 
 }
